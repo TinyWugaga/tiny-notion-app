9b046fb8ac7ae0029a727d2fec05b92fecfb9c83
Tiny <405401372@gapp.fju.edu.tw>
2021-10-06 17:20:34 +0800

    fix eslint error

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default class MyDocument extends Document {
     const initialProps = await Document.getInitialProps(ctx)
     return { ...initialProps }
   }
-  
+
   render() {
     return (
       <Html>
